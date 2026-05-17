41e382022b977fff15cc0b4fa36d5ced67393445
Lucas Huang <lucashuang@mac.mynetworksettings.com>
2026-05-17 11:55:56 -0400

    lucas lesson 3: variables, conditions, operators, basic gitops

---
+++ lessons/lesson_3.py
@@ -0,0 +1,73 @@
+# input_variable = input("Enter a number: ")
+# print(f"My input is {input_variable}")
+# temperature = 28
+# city = "Mclean"
+# print(f"{city}: {temperature} C")
+# raining = False
+# degree = 5.6
+# nonsense = None
+#
+# print(type(temperature))
+# print(type(city))
+#
+# print(degree)
+#
+# value = 20
+# print(value)
+# value = 34
+# print(value)
+#
+# PI = 3.1415926
+# MAX_TEMPERATURE = 100
+# ##############################
+# x = 10
+# x = x + 1
+# x += 1
+# # x = 12
+# y = 5
+# z = x ** y
+# print(z)
+#
+#
+# import sys
+# print(sys.maxsize)
+#
+#
+#
+# name = "Lucas"
+# greetings = "Hello"
+# print(greetings + ", " + name)
+# print(" Hi ".strip())
+# pod = "28"
+# print(type(int(pod)))
+# x = 5
+# print(x == 6)
+# print(x != 6)
+# print(x > 6)
+# print(x < 6)
+# print(x >= 5)
+# print(x <= 1)
+
+# User input a variable called 'name' from terminal
+# if the name is "Lucas", print "I am the boy"
+# if the name is 'Shuo', print "I am the big one"
+# if the name is 'Janice', print "I am mom"
+# if the name is 'Luna', print "I am the small one"
+# Else, print "I don't know you"
+
+
+name = input("What is your name?")
+if name == "Lucas":
+    print("I am the boy")
+
+elif name == "Shuo":
+    print("I am the big one")
+
+elif name == "Janice":
+    print("I am mom")
+
+elif name == "Luna":
+    print("I am the small one")
+
+else:
+    print("I don't know you")
